e68983177e1ea17a35f08c68e4bf201ffc5c05f5
Ahukko8 <ahukko88@gmail.com>
2023-05-28 18:00:16 +0500

    js code completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let moves = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 let currentComputerChoice = "";
+let round = 1;
 const results = [
     ["rock", "scissors", "win"],
     ["scissors", "paper","win"],
@@ -30,10 +31,31 @@ function myChoice(choice) {
         document.getElementById("result").innerHTML = "Tie!"
         console.log("tie")
     } else {
-        if (choice == "rock" && currentComputerChoice == "paper" || choice == "paper" && currentComputerChoice == "scissors" || choice == "scissors" && currentComputerChoice == "rock") {
-            document.getElementById("result").innerHTML = "You lose!"
+        const move = results.find(function(m){
+            return m[0] == choice && m[1] == currentComputerChoice;
+        });
+        if (move[2] == "win"){
+            if(move[0] == choice){
+                playerScore = playerScore+1;
+            }
         }
+        if(move[2] == "lose"){
+            if(move[0]== choice){
+                computerScore = computerScore+1;
+            }
+        }
+        let message = `you selected ${choice} computer selected ${currentComputerChoice} so you ${move[2]}`
+        document.getElementById('result').innerHTML = message;
     }
+    document.getElementById('playerScore').innerHTML = playerScore;
+    document.getElementById('computerScore').innerHTML = computerScore;
+    if(round == 5){
+        alert(`${playerScore > computerScore?"Player Wins!! YOU ROCK!!":"Computer Wins!! YOU SUCK!!"}`);
+        return location.reload() 
+    }
+    round = round+1;
+    document.getElementById('round').innerHTML = `Round ${round}`
+    getComputerChoice()
 }
 
 getComputerChoice()
